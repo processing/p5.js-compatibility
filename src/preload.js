b468a6a94ba2e51a152b7febbb0d610f37c715a4
Dave Pagurek <davepagurek@gmail.com>
2025-02-18 17:21:51 -0500

    Fix ordering to make sure functions are overridden before global binding

--- src/preload.js
+++ src/preload.js
@@ -6,40 +6,42 @@ function addPreload(p5, fn, lifecycles) {
     'loadStrings': () => [],
     'loadFont': (pInst) => new p5.Font(pInst, new FontFace('default', 'default.woff')),
   };
+
+  p5.isPreloadSupported = function() {
+    return true;
+  };
   
   const promises = [];
+  const prevMethods = {};
   
+  // Override existing methods to return an object immediately,
+  // and keep track of all things being loaded
+  for (const method in methods) {
+    const prevMethod = fn[method];
+    prevMethods[method] = prevMethod;
+
+    fn[method] = function(...args) {
+      if (!this._isInPreload) {
+        return prevMethod.apply(this, args);
+      }
+      const obj = methods[method](this);
+      const promise = prevMethod.apply(this, args).then((result) => {
+        for (const key in result) {
+          obj[key] = result[key];
+        }
+      });
+      promises.push(promise);
+      return obj;
+    }
+  }
+
   lifecycles.presetup = async function() {
     if (!window.preload) return;
-    
-    const promises = [];
-    const prevMethods = {};
-    
-    // Override existing methods to return an object immediately,
-    // and keep track of all things being loaded
-    for (const method in methods) {
-      const prevMethod = fn[method];
-      prevMethods[method] = prevMethod;
 
-      fn[method] = function(...args) {
-        const obj = methods[method](this);
-        const promise = prevMethod.apply(this, args).then((result) => {
-          for (const key in result) {
-            obj[key] = result[key];
-          }
-        });
-        promises.push(promise);
-        return obj;
-      }
-    }
-    
+    this._isInPreload = true;
     window.preload();
-    
-    // Restore previous functionality
-    for (const method in prevMethods) {
-      fn[method] = prevMethods[method];
-    }
-    
+    this._isInPreload = false;
+
     // Wait for everything to load before letting setup run
     await Promise.all(promises);
   }
